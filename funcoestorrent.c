860bb3fd8c7b23658d2045f047dd40fa84b4a611
Pedro Torrente <pedrotorrente@usp.br>
2021-07-16 15:01:59 -0300

    arrumando o contador de linhas e colunas e a leitura dos arquivos

--- funcoestorrent.c
+++ funcoestorrent.c
@@ -2,33 +2,31 @@
 #include <stdlib.h>
 #include<string.h>
 #include "funcoes.h"
-#define NUMCARACT (64)
+#define NUMCARACT (256)
 
 tabela *alocaDados (int nlin, int ncol) {
-    tabela *dados_pegos = malloc (sizeof(tabela) * 1);
-       if (dados_pegos == NULL){
+    tabela *dados_alocados = malloc (sizeof(tabela) * 1);
+       if (dados_alocados == NULL){
             printf("erro 1\n");
             exit(-1);
         }
-//    dados_pegos -> lin = num_linha;
-//    dados_pegos -> col = num_coluna;
-    dados_pegos -> dados = malloc (sizeof(char **) * nlin);
-       if (dados_pegos -> dados == NULL){
+    dados_alocados -> dados = malloc (sizeof(char **) * nlin);
+       if (dados_alocados -> dados == NULL){
             printf("erro 2\n");
             exit(-1);
         }
-    for (int i = 0; i < 101; i++){
-        dados_pegos -> dados[i] = malloc (sizeof(char *) * ncol);
-           if (dados_pegos -> dados[i] == NULL){
+    for (int i = 0; i < nlin; i++){
+        dados_alocados -> dados[i] = malloc (sizeof(char *) * ncol);
+           if (dados_alocados -> dados[i] == NULL){
                 printf("erro 3 na linha %d\n", i);
                 exit(-1);
             }
-        for (int j = 0; j < 6; j++){
-            dados_pegos -> dados[i][j] = malloc(sizeof(char) *  NUMCARACT);
+        for (int j = 0; j < ncol; j++){
+            dados_alocados -> dados[i][j] = malloc(sizeof(char) *  NUMCARACT);
         }
     }
 
-    return dados_pegos;
+    return dados_alocados;
 }
 
 void destruir_tabela (tabela *tabela_utilizada, int nlin, int ncol){
@@ -43,7 +41,7 @@ void destruir_tabela (tabela *tabela_utilizada, int nlin, int ncol){
 }
 
 
-void colocarColunasStruct (tabela *t, FILE *fd) {
+/*void colocarColunasStruct (tabela *t, FILE *fd) {
     char string_primeira_linha[200];
     fgets(string_primeira_linha, 200, fd);
     char* tok;
@@ -57,22 +55,22 @@ void colocarColunasStruct (tabela *t, FILE *fd) {
         tok = strtok(0, s);
         i++;
     }
-}
+}*/
 
 void pegaDados (tabela *tabela_alocada, FILE *fd){
     int i = 0;
     int j = 0;
     int k = 0;
-    int teste = 1;
-    colocarColunasStruct(tabela_alocada, fd);
+    int fimlinha = 0;
+    //colocarColunasStruct(tabela_alocada, fd);
     while(!feof(fd)){
-        while(teste && !feof(fd)){
+        while(!fimlinha && !feof(fd)){
             do{
                 tabela_alocada -> dados[i][j][k] = fgetc(fd);
                 k++;
             } while(tabela_alocada -> dados[i][j][k - 1] != '\t' && tabela_alocada -> dados[i][j][k-1] != '\n' && !feof(fd));
             if(tabela_alocada -> dados[i][j][k - 1] == '\n'){
-                teste = 0;
+                fimlinha = 1;
             }
             tabela_alocada -> dados[i][j][k] = '\0';
             k = 0;
@@ -80,50 +78,56 @@ void pegaDados (tabela *tabela_alocada, FILE *fd){
         }
         j = 0;
         i++;
-        teste = 1;
+        fimlinha = 0;
     }
 }
 
-void numLinColArquivo (char nome_do_arquivo[], int *numLin, int *numcol){
+void numLinColArquivo (FILE *fdlc, int *numLin, int *numcol){
     char c;
-    FILE *fd;
-    fd = fopen(nome_do_arquivo, "r");
     int lin = 0;
     int col = 0;
     int passou = 0;
-    while(!feof(fd)){
-        c = fgetc(fd);
-        if (c == '\n'){
-            passou = 1;
-            lin++;
+    while(!feof(fdlc)){
+        c = fgetc(fdlc);
+        if(!passou && (c == '\t' || c == '\n')){
+            col++;
         }
-        while (!passou) {
-            if (c == '\t') {
-                col++;
-            }
+
+        if(c == '\n'){
+            lin++;
+            passou = 1;
         }
     }
-    *numLin = lin;
+    
+    *numLin = lin + 1;
     *numcol = col;
 }
 
 int main (void){
+    FILE *fdlincol;
+    fdlincol = fopen("Trabalhos.tsv", "r"); //PRECISA DEIXAR O "Trabalhos.tsv" COMO PARÂMETRO UNIVERSAL
+    if (fdlincol == NULL){
+    fprintf(stderr, "Erro ao abrir o entradas.txt\n");
+    exit (-1);
+    }
+    int nlin, ncol;
+    numLinColArquivo(fdlincol, &nlin, &ncol);
+    fclose(fdlincol);
+    
     FILE *fd;
-    fd = fopen("Docentes.tsv", "r");
+    fd = fopen("Trabalhos.tsv", "r");
         if (fd == NULL){
         fprintf(stderr, "Erro ao abrir o entradas.txt\n");
         exit (-1);
     }
     tabela *docentes;
 
-    int nlin, ncol;
-    numLinColArquivo("Docentes.tsv", &nlin, &ncol);
     docentes = alocaDados(nlin, ncol);
     pegaDados(docentes, fd);
     
     for (int m = 0; m < nlin; m++){
         for (int l = 0; l < ncol; l++){
-            //printf("%s\n", docentes -> dados[m][l]);
+            printf("%s\n", docentes -> dados[m][l]);
         }
     }
     destruir_tabela(docentes, nlin, ncol);
